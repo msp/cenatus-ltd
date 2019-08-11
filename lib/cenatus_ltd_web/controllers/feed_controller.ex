90a9ae911dad66c05eb04f51012f997be4901660
msp <matt@cenatus.org>
2019-08-11 14:57:27 +0100

    Order the RSS feed by newest first

--- lib/cenatus_ltd_web/controllers/feed_controller.ex
+++ lib/cenatus_ltd_web/controllers/feed_controller.ex
@@ -2,18 +2,17 @@ defmodule CenatusLtdWeb.FeedController do
   use CenatusLtdWeb, :controller
 
   def index(conn, _params) do
-    posts =
-      Repo.all(
-        from(
-          p in CenatusLtdWeb.Article,
-          order_by: [desc: p.published_at],
-          preload: [:section, :category],
-          limit: 300
-        )
+    article_query =
+      from(article in CenatusLtdWeb.Article,
+        preload: [:section, :category],
+        order_by: [desc: article.published_at],
+        limit: 300
       )
+
+    articles = Repo.all(article_query)
 
     conn
     |> put_resp_content_type("application/xml")
-    |> render("index.xml", items: posts)
+    |> render("index.xml", items: articles)
   end
 end
